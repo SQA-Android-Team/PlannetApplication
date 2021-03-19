53dc026b02b885e2a169d7eaaa12a7064a7d5f60
Jordan Han <dunghoang1c.hanu@gmail.com>
2021-03-19 22:42:09 +0700

    Dung update

--- app/src/main/java/com/sqa/plannet/activity/subject/SubjectDetailActivity.java
+++ app/src/main/java/com/sqa/plannet/activity/subject/SubjectDetailActivity.java
@@ -1,28 +1,80 @@
 package com.sqa.plannet.activity.subject;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.sqa.plannet.R;
+import com.sqa.plannet.activity.teacher.TeacherDetailActivity;
 
 public class SubjectDetailActivity extends AppCompatActivity {
     ImageButton backBtn;
     ImageButton deleteBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.subject_detail);
 
-        // add event listener for backBtn
+        initUI();
+        onBackBtnClick();
+        onDeleteBtn();
+
+
+
+    }
+
+    /**
+     * TODO: initialise all necessary UI elements
+     */
+    private void initUI(){
         backBtn = findViewById(R.id.backBtn);
+        deleteBtn = findViewById(R.id.deleteBtn);
+    }
+    /**
+     * TODO:  add event listener for backBtn
+     */
+    private void onBackBtnClick() {
         backBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 finish();
+            }
+        });
+    }
+    /**
+     * TODO: add event listener for delete button
+     * INCOMPLETE
+     */
+    private void onDeleteBtn(){
+        deleteBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                AlertDialog deleteConfirm = new AlertDialog.Builder(SubjectDetailActivity.this)
+                        .setTitle("Confirmation")
+                        .setMessage("Do you really want to delete this ")
+                        .setIcon(R.drawable.ic_baseline_delete_24)
+                        .setPositiveButton("Delete", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int i) {
+                                // deleting code
+                                dialog.dismiss();
+                            }
+                        })
+                        .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int i) {
+                                dialog.dismiss();
+                            }
+                        })
+                        .create();
+
+                deleteConfirm.show();
+
             }
         });
     }
