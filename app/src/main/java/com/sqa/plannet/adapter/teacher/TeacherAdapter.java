bcd5418bf326359226c4533e0582921c17353265
Jordan Han <dunghoang1c.hanu@gmail.com>
2021-03-19 08:52:46 +0700

    Add TeacherAdapter

--- app/src/main/java/com/sqa/plannet/adapter/teacher/TeacherAdapter.java
+++ app/src/main/java/com/sqa/plannet/adapter/teacher/TeacherAdapter.java
@@ -1,4 +1,60 @@
 package com.sqa.plannet.adapter.teacher;
 
-public class TeacherAdapter {
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.sqa.plannet.R;
+import com.sqa.plannet.model.Teacher;
+
+import java.util.List;
+
+public class TeacherAdapter extends RecyclerView.Adapter<TeacherAdapter.TeacherViewHolder> {
+    private List<Teacher> listTeacher;
+
+    public void seData(List<Teacher> list){
+        this.listTeacher = list;
+        notifyDataSetChanged();
+    }
+
+
+    @NonNull
+    @Override
+    public TeacherViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.teacher_item, parent, false);
+        return new TeacherViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull TeacherAdapter.TeacherViewHolder holder, int position) {
+        Teacher teacher = listTeacher.get(position);
+        if (teacher == null){
+            return;
+        }
+
+        holder.teacherName.setText(teacher.getTeacherName());
+    }
+
+    @Override
+    public int getItemCount() {
+        if (this.listTeacher != null){
+            return this.listTeacher.size();
+        }
+        return 0;
+    }
+
+    public class TeacherViewHolder extends RecyclerView.ViewHolder {
+
+        private TextView teacherName;
+        public TeacherViewHolder(@NonNull View view){
+            super(view);
+            teacherName = view.findViewById(R.id.teacherNameTxv);
+        }
+
+
+    }
 }
